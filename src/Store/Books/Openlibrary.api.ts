88e4fac1308de4c3c2ca59b5eb3479b5a38a57c4
IvanStarenchenko <90762663+IvanStarenchenko@users.noreply.github.com>
2026-04-17 19:10:27 +0300

    test-push

--- src/Store/Books/Openlibrary.api.ts
+++ src/Store/Books/Openlibrary.api.ts
@@ -1,34 +1,46 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
-import { OpenLibraryBookDetails, OpenLibraryCoversResult, OpenLibraryGenre, OpenLibrarySearch } from './Openlibrary.type'
+import {
+	OpenLibraryBookDetails,
+	OpenLibraryCoversResult,
+	OpenLibraryGenre,
+	OpenLibrarySearch,
+} from './Openlibrary.type'
 
 const baseUrl = 'https://openlibrary.org/'
-
-
 
 export const openLibraryApi = createApi({
 	reducerPath: 'openLibraryApi',
 	tagTypes: ['books'],
 	baseQuery: fetchBaseQuery({
 		baseUrl,
-		fetchFn: (input: RequestInfo | URL, init?: RequestInit) => fetch(input, { ...init, next: { revalidate: 3600 } }),
+		fetchFn: (input: RequestInfo | URL, init?: RequestInit) =>
+			fetch(input, { ...init, next: { revalidate: 3600 } }),
 	}),
 	endpoints: builder => ({
-		searchBooks: builder.query<OpenLibrarySearch, { query: string; page?: number }>({
+		searchBooks: builder.query<
+			OpenLibrarySearch,
+			{ query: string; page?: number }
+		>({
 			query: ({ query, page = 1 }) => ({
 				url: 'search.json',
 				params: {
 					q: query,
 					page: page,
 					limit: 20,
-				}
-			})
+				},
+			}),
 		}),
-		getAuthorDetails: builder.query<{ name: string; photos?: number[]; bio?: string }, string>({
-			query: (authorKey) => `${authorKey}.json`,
+		getAuthorDetails: builder.query<
+			{ name: string; photos?: number[]; bio?: string },
+			string
+		>({
+			query: authorKey => `${authorKey}.json`,
 		}),
-		getBooksByGenre: builder.query<OpenLibraryGenre, { genre: string; page?: number; limit?: number }>({
+		getBooksByGenre: builder.query<
+			OpenLibraryGenre,
+			{ genre: string; page?: number; limit?: number }
+		>({
 			query: ({ genre, page = 1, limit = 20 }) => {
-
 				const offset = (page - 1) * limit
 
 				return {
@@ -36,38 +48,38 @@ export const openLibraryApi = createApi({
 					params: {
 						limit,
 						offset,
-						details: true
+						details: true,
 					},
-
 				}
-
 			},
-			providesTags: (result) => (result ? [{ type: 'books', id: 'LIST' }] : []),
+			providesTags: result => (result ? [{ type: 'books', id: 'LIST' }] : []),
 			keepUnusedDataFor: 3600,
-		})
-		,
+		}),
 		getBookDetails: builder.query<OpenLibraryBookDetails, string>({
-			query: (workId) => `works/${workId}.json`,
+			query: workId => `works/${workId}.json`,
 		}),
 		getBookCovers: builder.query<number[], string>({
-			query: (workId) => `works/${workId}/editions.json`,
+			query: workId => `works/${workId}/editions.json`,
 			transformResponse: (response: OpenLibraryCoversResult) => {
 				const allCovers = response.entries
-					.flatMap((entry: OpenLibraryCoversResult['entries'][number]) => entry.covers || [])
+					.flatMap(
+						(entry: OpenLibraryCoversResult['entries'][number]) =>
+							entry.covers || []
+					)
 					.filter((id: number) => id > 0)
 				return Array.from(new Set(allCovers)) as number[]
-			}
+			},
 		}),
 
 		searchByAuthor: builder.query<OpenLibrarySearch, string>({
-			query: (authorName) => ({
+			query: authorName => ({
 				url: 'search.json',
 				params: {
 					author: authorName,
-				}
-			})
+				},
+			}),
 		}),
-	})
+	}),
 })
 
 export const {
@@ -77,5 +89,5 @@ export const {
 	useGetBookDetailsQuery,
 	useGetAuthorDetailsQuery,
 	useGetBookCoversQuery,
-	useSearchByAuthorQuery
+	useSearchByAuthorQuery,
 } = openLibraryApi
