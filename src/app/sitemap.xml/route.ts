d7e55f1d10b9ce4990f5c50b84748a7498a02ff8
IvanStarenchenko <90762663+IvanStarenchenko@users.noreply.github.com>
2026-04-17 16:43:22 +0300

    xml

--- src/app/sitemap.xml/route.ts
+++ src/app/sitemap.xml/route.ts
@@ -139,17 +139,14 @@ export async function GET(req: Request) {
 
 	const allUrls = [...urls, ...dynamicUrls].filter(Boolean) as string[]
 
-	const xml = `<?xml version="1.0" encoding="UTF-8"?>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-${allUrls
-	.map(
-		url => `
-  <url>
-    <loc>${escapeXml(url)}</loc>
-  </url>`
-	)
-	.join('')}
-</urlset>`
+	const xmlStart =
+		'<?xml version="1.0" encoding="UTF-8"?><urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">'
+	const xmlEnd = '</urlset>'
+	const body = allUrls
+		.map(url => `<url><loc>${escapeXml(url)}</loc></url>`)
+		.join('')
+
+	const xml = `${xmlStart}${body}${xmlEnd}`
 
 	return new Response(xml, {
 		headers: {
