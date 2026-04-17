f21fb8cd649050136b90d627e815eaa0c538e5da
IvanStarenchenko <90762663+IvanStarenchenko@users.noreply.github.com>
2026-04-17 18:21:39 +0300

    idk

--- src/app/sitemap.xml/route.ts
+++ src/app/sitemap.xml/route.ts
@@ -16,19 +16,12 @@ export async function GET() {
 
 	const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-  ${urls
-		.map(
-			url => `
-    <url>
-      <loc>${url}</loc>
-    </url>`
-		)
-		.join('')}
+${urls.map(url => `<url><loc>${url}</loc></url>`).join('')}
 </urlset>`.trim()
 
 	return new Response(sitemap, {
 		headers: {
-			'Content-Type': 'application/xml',
+			'Content-Type': 'application/xml; charset=utf-8',
 			'Cache-Control': 'public, s-maxage=3600, stale-while-revalidate=59',
 		},
 	})
