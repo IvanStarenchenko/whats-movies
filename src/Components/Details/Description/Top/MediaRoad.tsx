ae18b1dd9201ccc2042a05970041cf659fd490f7
IvanStarenchenko <90762663+IvanStarenchenko@users.noreply.github.com>
2026-04-27 02:31:38 +0300

    changed path

--- src/Components/Details/Description/Top/MediaRoad.tsx
+++ src/Components/Details/Description/Top/MediaRoad.tsx
@@ -24,7 +24,7 @@ export function MediaRoad({ title }: MediaRoadProps) {
 				
 '
 			>
-				<Link href={`https://road.media-hub.lol/${slugPath}`}>
+				<Link href={`https://road.media-hub.lol/roadmaps/${slugPath}`}>
 					<Map />
 
 					<p
