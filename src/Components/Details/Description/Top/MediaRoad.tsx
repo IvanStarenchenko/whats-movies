e446285c54daf67ea57755ac1bcdc5096a88c9ab
IvanStarenchenko <90762663+IvanStarenchenko@users.noreply.github.com>
2026-04-27 21:47:58 +0300

    test

--- src/Components/Details/Description/Top/MediaRoad.tsx
+++ src/Components/Details/Description/Top/MediaRoad.tsx
@@ -24,7 +24,7 @@ export function MediaRoad({ title }: MediaRoadProps) {
 				
 '
 			>
-				<Link href={`https://road.media-hub.lol/roadmaps/${slugPath}`}>
+				<Link href={`/`}>
 					<Map />
 
 					<p
